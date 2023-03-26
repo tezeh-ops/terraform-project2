b98a3fb45de9f31cac7590c1b64a2c8480e174ee
samuel Tezeh <samuelmunoh@gmail.com>
2023-03-26 07:50:21 +0000

    six commit

--- .github/workflows/provider.tf
+++ .github/workflows/provider.tf
@@ -6,8 +6,4 @@ terraform {
   }
 }
 
-provider "aws"  {
 
-  region  = "us-east-1"
- profile = "default" //uses default password profile from AWS credentials file
-} 
--- provider.tf
+++ provider.tf
@@ -6,8 +6,7 @@ terraform {
   }
 }
 
-provider "aws" {
-
-  region  = "us-east-1"
-  profile = "default" //uses default password profile from AWS credentials file
-} 
+provider "aws"  {
+    region = "us-east-1"
+    #default = "default"
+}
